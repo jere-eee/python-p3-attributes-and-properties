c16305a8272677a80ea0dfcfb71e20fa841e7f48
Akanle Jeremy <akanlejeremy@gmail.com>
2024-11-26 14:47:48 +0300

    attributes and props

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,28 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Bob", breed="Mastiff") -> None:
+        self.name = name
+        self.breed = breed
+        
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter  
+    def name(self, n):
+        if isinstance(n, str) and 1 <= len(n) <= 25:
+            self._name = n
+        else:
+            print('Name must be string between 1 and 25 characters.')
+            
+    @property
+    def breed(self):
+        return self._breed
+    
+    @breed.setter
+    def breed(self, breed):
+        if breed in APPROVED_BREEDS:
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")   
